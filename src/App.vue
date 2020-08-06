29f10fe4255ef236d7d28637ce31f897d2e24f92
baiaimin <15130794497@163.com>
2020-08-06 17:21:22 +0800

    提交完成vdom简单实现--白爱民--2020年8月6日17:21:20

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import { createElement } from "../src/utils/valculator/element";
+import { renderDom } from "../src/utils/valculator/element";
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+  },
+  mounted() {
+    this.VDOMFunction();
+  },
+  methods: {
+    VDOMFunction() {
+      let V_DOM = createElement("ul", { class: "list" }, [
+        createElement("li", { class: "item" }, ["item1"]),
+        createElement("li", { class: "item" }, ["item2"]),
+        createElement("li", { class: "item" }, ["item3"]),
+      ]);
+
+      console.log(V_DOM);
+      var el = renderDom(V_DOM);
+      console.log(el);
+      document.body.appendChild(el); // 向elemente最后一个节点后添加此内容
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
